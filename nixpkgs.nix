53c9175cb36cc710abbc6bde5ef52eddd21be83b
Mathew Polzin <matt.polzin@gmail.com>
2024-09-02 22:27:14 -0500

    use flake lock owner and repo for nixpkgs git fetch for no particular reason

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,8 +1,10 @@
 let flakeLock = builtins.fromJSON (builtins.readFile ./flake.lock);
   rev = flakeLock.nodes.nixpkgs.locked.rev;
   hash = flakeLock.nodes.nixpkgs.locked.narHash;
+  owner = flakeLock.nodes.nixpkgs.locked.owner;
+  repo = flakeLock.nodes.nixpkgs.locked.repo;
 in
 import (builtins.fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
+  url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
   sha256 = hash;
 }) {}
