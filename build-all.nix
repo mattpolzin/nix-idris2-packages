184d79ccad3d9cd625dd8b000f069aee5ad1fc39
Mathew Polzin <matt.polzin@gmail.com>
2024-09-07 20:31:32 -0500

    fix building of all packages in CI given some don't support all platforms anymore

--- build-all.nix
+++ build-all.nix
@@ -2,8 +2,9 @@
   pkgs ? import ./nixpkgs.nix,
 }:
 let
+  supportedPlatform = attrs: !(attrs.meta ? "platforms") || builtins.elem pkgs.stdenv.hostPlatform.config attrs.meta.platforms;
   idris2Packages = pkgs.callPackage ./. { };
-  packages = pkgs.lib.filterAttrs (n: attrs: !attrs.meta.broken) idris2Packages.packages;
+  packages = pkgs.lib.filterAttrs (n: attrs: !attrs.meta.broken && supportedPlatform attrs) idris2Packages.packages;
 in
 pkgs.runCommand "all-packages"
   {
