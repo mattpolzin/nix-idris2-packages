ddb6cb6f44bb0dab9c0db421af735fc0fe917bbf
Mathew Polzin <matt.polzin@gmail.com>
2024-09-30 23:12:10 -0500

    use previous commit for example

--- examples/non-flake-without-source-with-source/packageset.nix
+++ examples/non-flake-without-source-with-source/packageset.nix
@@ -7,6 +7,6 @@
 }:
 import (pkgs.fetchgit {
   url = "https://github.com/mattpolzin/nix-idris2-packages";
-  rev = "d2ed7713115e74bad2d78ac9f73d083010028366";
-  hash = "sha256-AE7a6ngPRbLLMn6DGH9GIaCWVEbqVVIRVU2rg+nj2eU=";
+  rev = "a1319bac424e7c1005ca676ddfdf0b73e0e17e1d";
+  hash = "sha256-v5KP15EBoDsNR1QhwOSRWTIDsNBqTcdiOAUK9/4xMos=";
 }) { inherit withSource; }
